84cbf22640ed96c0f60cae6aae4961819bd48101
prashantdalvi <61616275+prashantdalvi@users.noreply.github.com>
2020-11-12 16:38:37 +0530

    new file added
    new file added

---
+++ TenExcelUploading/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ExcelimportComponent } from './excelimport/excelimport.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExcelimportComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
